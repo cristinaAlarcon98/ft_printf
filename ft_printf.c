4d8f22f796a0bdb68f91feba9d430ae8bfe32c8d
Cristina Alarcon <cralarco@c1r5s5.42london.com>
2025-03-05 15:24:17 +0000

    first version

--- ft_printf.c
+++ ft_printf.c
@@ -2,14 +2,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int ft_caca(const char *str, ...)
+int ft_printf(const char *str, ...)
 {
     
     va_list args;
     va_start(args, str);
 
-    const char *current_str = str;
-    while(current_str)
+    const char *current_str;
+    current_str = str;
+    while(*current_str)
     {
         printf("%s", current_str);
         current_str = va_arg(args, const char*);
@@ -18,7 +19,7 @@ int ft_caca(const char *str, ...)
     return 0;
 }
 
-void main()
+int main()
 {
-    ft_caca("HOLA","ME","LLAMO");
+    ft_printf("HOLA","ME","LLAMO");
 }
