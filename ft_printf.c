47bfb3a025da3312511c97d77f673c1bce3db21a
Cristina Alarcon <cralarco@c1r5s5.42london.com>
2025-03-07 14:17:06 +0000

    added logic to print integers and chars

--- ft_printf.c
+++ ft_printf.c
@@ -1,25 +1,76 @@
 
-#include <stdarg.h>
-#include <stdio.h>
+#include "ft_printf.h"
+
+
+int get_num_vars(const char *str)
+{
+    int counter;
+    int i;
+
+    if(!str)
+        return(0);
+    counter = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if(str[i] == '%')
+            counter++;
+        i++;
+    }
+    return (counter);
+    
+}
+
+char *get_next_type(char *str)
+{
+    
+
+    while(*str != '\0')
+    {
+        if(*str == '%')
+        {
+            str++;
+            return (str);
+        }
+        str++;
+    }
+}
 
 int ft_printf(const char *str, ...)
 {
     
+    
+    int num_vars;
+    int i;
+    char *buff;
+    int int_ptr;
+    char *str_pointer;
+
     va_list args;
     va_start(args, str);
-
-    const char *current_str;
-    current_str = str;
-    while(*current_str)
+    
+    num_vars = get_num_vars(str);
+    i = 0;
+    str_pointer = (char *)str;
+    while(i < num_vars)
     {
-        printf("%s", current_str);
-        current_str = va_arg(args, const char*);
+        str_pointer = get_next_type(str_pointer);
+        handle_input(str_pointer, args);
+        i++;
     }
     va_end(args);
     return 0;
 }
 
+void handle_input(char *str_pointer, va_list args)
+{
+    if (str_pointer[0] == 's')
+        ft_put_str(va_arg(args, char*), 1);
+    if (str_pointer[0] == 'd')
+        ft_putnbr_fd(va_arg(args, int), 1);
+}
+
 int main()
 {
-    ft_printf("HOLA","ME","LLAMO");
+    ft_printf("Var num %d  %s", 12, "hola");
 }
---
+++ ft_printf.h
@@ -0,0 +1,18 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putnbr_fd(int n, int fd);
+int ft_printf(const char *str, ...);
+void	ft_putchar_fd(char c, int fd);
+char *get_next_type(char *str);
+int get_num_vars(const char *str);
+void	ft_put_str(char *str, int fd);
+void handle_input(char *str_pointer, va_list args);
+
+
+
+#endif
